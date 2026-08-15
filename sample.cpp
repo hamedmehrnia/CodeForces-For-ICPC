ce10bffb86bb2b9f8f026cf75fb153392d916c4d
Soghan <hamed.mehrnia.2006@gmail.com>
2026-08-15 04:35:41 +0330

    add sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(start, end) for (long long i = start; i < end; i++)
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0);
+
+typedef long long ll;
+
+int main() {
+    FAST;
+    
+    
+    return 0;
+}
